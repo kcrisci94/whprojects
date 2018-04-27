55f7050208002b8a0f041f5a7e52fc128fcd59fc
Kaleb Crisci <kcrisci94@gmail.com>
2018-04-27 13:58:29 -0700

    adding portfolio items using js

--- portfolio/javascript/java.js
+++ portfolio/javascript/java.js
@@ -1,1 +1,22 @@
 
+var portItems = document.querySelector(".portfolioDisplay ul");
+var portPages = [
+    {
+    name: "59DaysOfCodeTemplate",
+    image: "../pictures/59daysOfCodeLogo.PNG",
+    pageUrl: "https://kcrisci94.github.io/whprojects/mar05/Template/template.html"
+    }
+    // {
+        
+    // }
+];
+
+function displayPortItems() {
+    for(var i = 0; i < portPages.length; i++) {
+        var li = `
+            <li><a href="${portPages[i].pageUrl}"><div><img src="${portPages[i].image}" alt="${portPages[i].name}" /><h3>${portPages[i].name}</div></h3></a></li>
+        `;
+        portItems.innerHTML += li;
+    }
+}
+displayPortItems();
