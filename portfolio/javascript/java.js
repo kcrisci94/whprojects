b83a67803a275682cb089f8f7308c81007cb9400
Kaleb Crisci <kcrisci94@gmail.com>
2018-04-29 09:45:30 -0700

    added pages to portfolio

--- portfolio/javascript/java.js
+++ portfolio/javascript/java.js
@@ -5,16 +5,28 @@ var portPages = [
     name: "59DaysOfCodeTemplate",
     image: "../pictures/59daysOfCodeLogo.PNG",
     pageUrl: "https://kcrisci94.github.io/whprojects/mar05/Template/template.html"
+    }, 
+    {
+        name: "NumberGuessingGame",
+        image: "../pictures/numberGuessingGame.PNG",
+        pageUrl: "https://kcrisci94.github.io/whprojects/april06/index.html"
+    }, 
+    {
+        name: "PhotosForSaleTemplate",
+        image: "../pictures/photosForSale.PNG",
+        pageUrl: "https://kcrisci94.github.io/whprojects/april13/index.html"
+    },
+    {
+        name: "RickandMortyPlayingCards",
+        image: "../pictures/rickAndMorty.PNG",
+        pageUrl: "https://kcrisci94.github.io/whprojects/april20/index.html"
     }
-    // {
-        
-    // }
 ];
 
 function displayPortItems() {
     for(var i = 0; i < portPages.length; i++) {
         var li = `
-            <li><a href="${portPages[i].pageUrl}"><div><img src="${portPages[i].image}" alt="${portPages[i].name}" /><h3>${portPages[i].name}</div></h3></a></li>
+            <li><a href="${portPages[i].pageUrl}"><div><img src="${portPages[i].image}" alt="${portPages[i].name}" /><h3>${portPages[i].name}</h3></div></a></li>
         `;
         portItems.innerHTML += li;
     }
