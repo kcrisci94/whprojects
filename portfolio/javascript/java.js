8cef3efb52abab55ba82e17876dcf2c2ef00c0d2
Kaleb Crisci <kcrisci94@gmail.com>
2018-04-30 12:28:35 -0700

    screen resizing

--- portfolio/javascript/java.js
+++ portfolio/javascript/java.js
@@ -2,22 +2,22 @@
 const portItems = document.querySelector(".portfolioDisplay ul");
 const portPages = [
     {
-    name: "59DaysOfCodeTemplate",
+    name: "59 Days Of Code Template",
     image: "pictures/59daysOfCodeLogo.jpg",
     pageUrl: "https://kcrisci94.github.io/whprojects/mar05/Template/template.html"
     }, 
     {
-        name: "NumberGuessingGame",
+        name: "Number Guessing Game",
         image: "pictures/numberGuessingGame.jpg",
         pageUrl: "https://kcrisci94.github.io/whprojects/april06/index.html"
     }, 
     {
-        name: "PhotosForSaleTemplate",
+        name: "Photos For Sale Template",
         image: "pictures/photosForSale.jpg",
         pageUrl: "https://kcrisci94.github.io/whprojects/april13/index.html"
     },
     {
-        name: "RickandMortyPlayingCards",
+        name: "Rick and Morty Playing Cards",
         image: "pictures/rickAndMorty.jpg",
         pageUrl: "https://kcrisci94.github.io/whprojects/april20/index.html"
     }
