430fd14668be18cf3c0dee75027677cf566feda0
Kaleb Crisci <kcrisci94@gmail.com>
2018-06-13 16:22:37 -0700

    new portfolio

---
+++ portfolio2/js/script.js
@@ -0,0 +1,43 @@
+const portItems = document.querySelector("#projects ul");
+const portPages = [
+    {
+        name: "59 Days Of Code Template",
+        image: "pictures/59daysOfCodeLogo.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/mar05/Template/template.html"
+    }, 
+    {
+        name: "Photos For Sale Template",
+        image: "pictures/photosForSale.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/april13/index.html"
+    },
+    {
+        name: "Tribute Template",
+        image: "pictures/tributePage.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/portfolioItems/tributeTemplate/index.html"
+    }, 
+    {
+        name: "Sample Survey Form",
+        image: "pictures/survey.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/portfolioItems/survey/index.html"
+    },
+    {
+        name: "Product Landing Page",
+        image: "pictures/productLanding.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/portfolioItems/productLandingPage/index.html"
+    },
+    {
+        name: "Technical Document Outline",
+        image: "pictures/technicalDocument.jpg",
+        pageUrl: "https://kcrisci94.github.io/whprojects/portfolioItems/technicalDocument/index.html"
+    }
+];
+
+function displayPortItems() {
+    for(let i = 0; i < portPages.length; i++) {
+        let li = `
+            <li><a href="${portPages[i].pageUrl}" target="_blank"><div class="project-tile"><img src="${portPages[i].image}" alt="${portPages[i].name}" /><p>${portPages[i].name}</p></div></a></li>
+        `;
+        portItems.innerHTML += li;
+    }
+}
+displayPortItems();
